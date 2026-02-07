4069c5e4adee6772be5d0927ba9a17a7571533eb
dii08-art <danielrayb06@gmail.com>
2026-02-06 23:30:59 -0500

    Update config.jsFOUR

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🌺', '˚⊱🪷⊰˚']                       // Cute bear emojis
+        hearts: ['❤️', '💖', '💗', '💓'],  // Heart emojis
+        bears: ['🌺', '˚🪷˚']                       // Cute bear emojis
     },
 
     // Questions and answers
